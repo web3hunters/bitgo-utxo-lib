3908f36893799a40b2669f126e5e82438d0b55c3
michaeltout <toutonghi.michael@gmail.com>
2022-02-24 00:17:53 +0100

    Update Zcash consensus branch ID

--- src/networks.js
+++ src/networks.js
@@ -110,7 +110,7 @@ module.exports = {
       1: 0x00,
       2: 0x00,
       3: 0x5ba81b19,
-      4: 0xf5b9230b
+      4: 0x37519621
     },
     coin: coins.ZEC,
     isZcash: true
@@ -129,7 +129,7 @@ module.exports = {
       1: 0x00,
       2: 0x00,
       3: 0x5ba81b19,
-      4: 0xf5b9230b
+      4: 0x37519621
     },
     coin: coins.ZEC,
     isZcash: true
